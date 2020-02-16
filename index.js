3cd0fdc9ceef8fb0395feac8b477c4f7e3db044e
myk245 <myk245@nyu.edu>
2020-02-16 17:05:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let greeting
+  if (feet <= 400) {
+    greeting = "This one is on me!"; 
+  }
+  else if (feet > 2000 && feet < 2500) {
+    greeting = "I will gladly take your thirty bucks."; 
+  }
+  else {
+    greeting = "No can do.";
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let reply
+  switch(tip){
+    case "generous":
+      reply = "Thank you so much."; 
+      break;
+    case "not as generous":
+      reply = "Thank you.";
+      break; 
+    default:
+      reply = "Bye."
+  }
+  return reply
 }
