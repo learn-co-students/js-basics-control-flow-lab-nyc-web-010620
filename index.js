79a1b8da7fe622fee158b54449a4c3a0d23ed9d2
Kate Raskauskas <katherine.raskauskas@gmail.com>
2020-02-17 23:57:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let response;
+  if (feet < 400) {
+    response = 'This one is on me!';
+  } else if (feet > 2500) {
+    response = 'No can do.';
+  } else if (feet >= 2000) {
+    response = 'I will gladly take your thirty bucks.';
+  };
+  return response;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
+  let response;
+  destination === "NYC" ? (response = "Ok, sounds good.") : (response = "No go.");
+  return response
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+
+  switch(tipAmount) {
+  case "generous":
+    return "Thank you so much."
+  case "not as generous":
+    return "Thank you."
+  default:
+    return "Bye."
+  }
 }
